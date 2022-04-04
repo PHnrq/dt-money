62693ecace577e140acd43b89876ba820f4333e7
PHnrq <paulofigueredo7@gmail.com>
2022-04-04 09:05:54 -0300

    fix cards size

--- src/components/Main/Cards/style.ts
+++ src/components/Main/Cards/style.ts
@@ -3,5 +3,5 @@ import  styled  from 'styled-components';
 export const CardStyle = styled.section`
     display: flex;
     gap: 3.2rem;
-    justify-content: center;
+    justify-content: space-between;
 `
