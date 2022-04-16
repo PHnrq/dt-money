2077b7ad046b5c9b96d1809e0c64510b5c009533
PHnrq <paulofigueredo7@gmail.com>
2022-04-16 06:12:10 -0300

    add new object for transaction

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,14 @@ createServer({
           amount: 1000,
           createdAt: new Date('2021-02-12 09:00:00'),
         },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'outcome',
+          category: 'Home',
+          amount: 350,
+          createdAt: new Date('2021-02-18 09:00:00'),
+        }
       ]
     })
   },
